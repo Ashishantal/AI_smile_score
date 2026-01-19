097f1676f4e46f578fd8677a2c507a389c8752f5
Ashish Antal <ashishash9015@gmail.com>
2026-01-19 16:20:34 +0530

    Update changes including file rename

--- main/settings.py
+++ main/settings.py
@@ -19,15 +19,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n$u(3i@4slh2!ali(cwq-5bb!*71_b@9j!k6o$nd+0y(0tn66o'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = os.environ.get("DEBUG") == "True"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 # Application definition
 
